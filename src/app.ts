bdf79143c27d5c35dc49964539db827fce8e61f8
JulienDuf <dufresne_julien@hotmail.ca>
2017-02-05 10:09:52 -0500

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,60 @@
+'use strict';
+
+import * as express from 'express';
+import * as path from 'path';
+import * as logger from 'morgan';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
+import { Index } from './route/index';
+
+const cors = require("cors");
+
+export class Application {
+
+    public app: express.Application;
+
+    public static bootstrap(): Application {
+        return new Application();
+    }
+
+    constructor() {
+        this.app = express();
+
+        this.config();
+
+        this.routes();
+    }
+
+    private config() {
+
+        this.app.set("views", path.join(__dirname, "../views"));
+        this.app.set("view engine", "pug");
+
+        this.app.use(logger('dev'));
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(cookieParser());
+        this.app.use(express.static(path.join(__dirname, '../../client')));
+        this.app.use(express.static(path.join(__dirname, '../public')));
+        this.app.use(cors());
+    }
+
+    public routes() {
+
+        let index: Index = new Index();
+        this.app.use("/", index.router);
+
+        this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+            let err = new Error('Not Found');
+            next(err);
+        });
+
+        this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+            res.status(err.status || 404);
+            res.send({
+                message: err.message,
+                error: {}
+            });
+        });
+    }
+}
